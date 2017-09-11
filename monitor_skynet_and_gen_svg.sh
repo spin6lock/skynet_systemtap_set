8a826e92ac7691ecd348da3893e789d944c096b0
spin6lock <spin6lock@users.noreply.github.com>
2017-09-11 16:47:44 +0800

    rm outdate stap; update monitor script

--- monitor_skynet_and_gen_svg.sh
+++ monitor_skynet_and_gen_svg.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-sudo stap mini_lua_bt.stp -p `pgrep -f "skynet.*center"` -u > a.bt
+sudo stap mini_lua_bt.stp -p `pgrep -f "skynet.*"` -u > a.bt
 ./stackcollapse.lua a.bt >a.cbt
 ./flamegraph.pl a.cbt >skynet.svg
