d74a035d726ef04ba26ad79a5f7ed8c2e25d2c57
spin6lock <spin6lock@users.noreply.github.com>
2018-09-08 16:47:08 +0800

    passing bin path in cmdline args, not in script

--- monitor_skynet_and_gen_svg.sh
+++ monitor_skynet_and_gen_svg.sh
@@ -1,4 +1,8 @@
 #/bin/sh
-sudo stap mini_lua_bt.stp -p `pgrep -f "skynet.*"` -u > a.bt
-./stackcollapse.lua a.bt >a.cbt
+if [ "$1" = "" ] | [ "$2" = "" ]; then
+    echo "usage: monitor pid skynet_bin_path"
+    exit 1
+fi
+sudo stap mini_lua_bt.stp --skip-badvars -x $1 -u $2 > a.bt
+lua stackcollapse.lua a.bt >a.cbt
 ./flamegraph.pl a.cbt >skynet.svg
